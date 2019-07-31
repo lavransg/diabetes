15668519217eba2751a4b50bef07718335fb05ca
Lavrans Grønseth <lavrans.gronseth@gmail.com>
2019-07-31 13:28:16 +0200

    removed angular material as it did not fit problem, implemented nested tree structure in edit page

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../question.service';
 
+export class Node {
+  children: Node[];
+}
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  originalData = this.questionService.data;
+  data = JSON.parse(JSON.stringify(this.questionService.data));
 
-  objectKeys = Object.keys
-  data: any[];
   constructor(private questionService: QuestionService) {
   }
 
   ngOnInit() {
-    if (this.questionService.data) { this.data = this.questionService.data; }
+    if (this.questionService.data) {
+      /* this.data = this.questionService.data; */
+    }
   }
 
 }
