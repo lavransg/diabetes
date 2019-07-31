dec79e3d69e44ae92434613f6cda421a28320acc
Lavrans Grønseth <lavrans.gronseth@gmail.com>
2019-07-31 15:59:15 +0200

    added function that updates data object with new weights on user input

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -22,4 +22,28 @@ export class EditComponent implements OnInit {
     }
   }
 
+  editQuestionAlternativeWeight(questionID,alternativeID,index,value){
+    let question = this.originalData["questions"].find(question => question.id == questionID)
+    let alternative = question.alternatives.find(alternative => alternative.id == alternativeID)
+    alternative.weights[index] = parseInt(value)
+
+    let question2 = this.data["questions"].find(question => question.id == questionID)
+    let alternative2 = question2.alternatives.find(alternative => alternative.id == alternativeID)
+    alternative2.weights[index] = parseInt(value)
+
+    console.log(this.originalData)
+  }
+
+  editTestAlternativeWeight(testID,alternativeID,index,value){
+    let test = this.originalData["tests"].find(test => test.id == testID)
+    let alternative = test.alternatives.find(alternative => alternative.id == testID)
+    alternative.weights[index] = parseInt(value)
+
+    let test2 = this.data["tests"].find(test => test.id == testID)
+    let alternative2 = test2.alternatives.find(alternative => alternative.id == alternativeID)
+    alternative2.weights[index] = parseInt(value)
+
+    console.log(this.originalData)
+  }
+
 }
